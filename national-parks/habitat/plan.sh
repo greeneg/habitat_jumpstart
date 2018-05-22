7c462f37a11179ca62db7ffde97f109d87699605
Habitat User <hab@ip-10-0-1-225.us-west-2.compute.internal>
2018-05-22 20:06:51 +0000

    Commit changes
    Signed-off-by: Habitat User <hab@ip-10-0-1-225.us-west-2.compute.internal>

--- national-parks/habitat/plan.sh
+++ national-parks/habitat/plan.sh
@@ -3,7 +3,7 @@ pkg_origin=greeneg-first-origin
 pkg_version="7.3.0"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("Apache-2.0")
-pkg_deps=(core/tomcat8 core/jre8)
+pkg_deps=(core/tomcat8 core/jre8 core/mongo-tools)
 pkg_build_deps=(core/jdk8/8u131 core/maven)
 pkg_svc_user="root"
 
@@ -11,15 +11,19 @@ do_prepare(){ export JAVA_HOME=$(hab pkg path core/jdk8) ; }
 
 do_build()
 {
-    cp -r $PLAN_CONTEXT/../ $HAB_CACHE_SRC_PATH/$pkg_dirname
+    cp -rv $PLAN_CONTEXT/../ $HAB_CACHE_SRC_PATH/$pkg_dirname
     cd ${HAB_CACHE_SRC_PATH}/${pkg_dirname}
     mvn package
 }
 
 do_install()
 {
-    mkdir ${PREFIX}/config
-    cp ${HAB_CACHE_SRC_PATH}/${pkg_dirname}/target/${pkg_name}.war ${PREFIX}/
-    cp $(hab pkg path core/tomcat8)/config/conf_server.xml ${PREFIX}/config/
+    mkdir -v ${PREFIX}/config
+    cp -v ${HAB_CACHE_SRC_PATH}/${pkg_dirname}/target/${pkg_name}.war ${PREFIX}/
+    cp -v $(hab pkg path core/tomcat8)/config/conf_server.xml ${PREFIX}/config/
+    cp -v ${HAB_CACHE_SRC_PATH}/${pkg_dirname}/data/national-parks.json ${PREFIX}/
 }
 
+pkg_binds=(
+    [database]="port"
+)
